2df6406a1d991ee9515dfd0eb1d63414c9ebb004
StevenNdaye <stevenndaye@gmail.com>
2016-04-22 06:22:39 +0200

    Steven: added a controller method to get all the customers

--- src/main/java/com/thoughtworks/spring/controllers/CustomerController.java
+++ src/main/java/com/thoughtworks/spring/controllers/CustomerController.java
@@ -1,9 +1,13 @@
 package com.thoughtworks.spring.controllers;
 
+import com.thoughtworks.spring.entities.Customer;
 import com.thoughtworks.spring.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/customers")
@@ -12,5 +16,10 @@ public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
 
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> getCustomers() {
+        return (List<Customer>) customerRepository.findAll();
+    }
 
 }
---
+++ src/main/resources/customer.sql
@@ -0,0 +1,2 @@
+INSERT INTO customer (id, firstName, lastName, birthDate, enrollment, status)
+VALUES (1, 'Steven', 'Ndaye', 26/06/1987, 13/01/2007, 'DEBT');
