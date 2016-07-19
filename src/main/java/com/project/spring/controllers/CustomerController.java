d4857ce723f50459772634d97f5b3b8f3d0b9403
StevenNdaye <stevenndaye@gmail.com>
2016-07-19 23:56:20 +0200

    Applied some modifications

--- src/main/java/com/project/spring/controllers/CustomerController.java
+++ src/main/java/com/project/spring/controllers/CustomerController.java
@@ -14,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 import java.util.List;
 
+import static com.project.spring.repository.Repository.Specs.birthDateIsBefore;
+import static com.project.spring.repository.Repository.Specs.birthDateIsBetween;
+import static com.project.spring.repository.Repository.Specs.firstNameIs;
+import static com.project.spring.repository.Repository.Specs.lastNameIs;
+import static com.project.spring.repository.Repository.Specs.orderByEnrollmentDesc;
+import static com.project.spring.repository.Repository.Specs.statusIn;
 import static org.springframework.data.jpa.domain.Specifications.where;
 
 @RestController
